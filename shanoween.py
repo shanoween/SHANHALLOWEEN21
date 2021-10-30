37941c00ed392997b986e57989bdc8500db48393
shanoween <93371853+shanoween@users.noreply.github.com>
2021-10-31 01:22:33 +0800

    Add files via upload

---
+++ shanoween.py
@@ -0,0 +1,30 @@
+import json
+import vigistheGOAT
+import random 
+import math
+import staggering_genius 
+
+stream_consciousness = {}
+global stream_consciousness
+
+with json.open(/C/Users/vigne/ProjectVision/PreciousThots.json) as immortal:
+	vig_conciousness = json.load(immortal)
+
+n = 0
+while True:
+	for obj in vig_conciousness
+	stream_consciousness[n] = obj
+	n+=1
+
+class VigTransferszn(object):
+	def __init__(self):
+		super.__init__(vigistheGOAT)
+	def build_a_vig(self):
+		if (stream_consciousness != None):
+			vigistheGOAT.activateImmortality(mind_state)
+
+NeWViG = VigTransferszn()
+#execute build_a_vig
+print("Hello World")
+NeWViG.build_a_vig()
+print("MWAHAHAHAHAH, FK CHICKENS")
